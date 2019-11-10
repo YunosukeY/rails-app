e1a386a02e90d6f9676e10e4a72bf714828e2e9d
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2019-11-10 19:44:51 +0900

    done 9.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
+  # /にアクセスされたらホームを見せる
   root 'static_pages#home'
+
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  # /users以下のアクションを規定
   resources :users
 end
